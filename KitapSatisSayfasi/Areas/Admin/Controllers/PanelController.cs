25190949c9e16e71d752b6ed508ca57cfbcd835d
ÖmerPirinçcioğlu <100633352+OmerPirinccioglu@users.noreply.github.com>
2022-07-07 14:44:15 +0300

    Initial commit

---
+++ KitapSatisSayfasi/Areas/Admin/Controllers/PanelController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KitapSatisSayfasi.Areas.Admin.Controllers
+{
+    [Authorize(Roles ="Admin")]//sadece admin olan girebilir demek
+    [Area("Admin")]
+    public class PanelController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ KitapSatisSayfasi/Models/Rol.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace KitapSatisSayfasi.Models
+{
+    public class Rol:IdentityRole<int>
+    {
+       
+    }
+}
